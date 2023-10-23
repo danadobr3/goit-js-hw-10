dc60780b869b7fccdd0c2983a17a9ab82c1f5bff
danadobr3 <134941956+danadobr3@users.noreply.github.com>
2023-10-23 21:24:34 +0200

    final 2

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import { fetchBreeds, fetchCatByBreed } from "./js/cat-api";
+import { fetchBreeds, fetchCatByBreed } from "./cat-api";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SlimSelect from 'slim-select'
 import 'slim-select/dist/slimselect.css';
