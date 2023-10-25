d6db77f1829edb1e09460a5f6070ab748aeece90
danadobr3 <134941956+danadobr3@users.noreply.github.com>
2023-10-25 16:49:38 +0200

    try to fix

--- src/js/index.js
+++ src/js/index.js
@@ -61,8 +61,6 @@ function onSelectBreed(event) {
 function onFetchError(error) {
     selector.classList.remove('is-hidden');
     loader.classList.replace('loader', 'is-hidden');
-    divCatInformation.classList.add('is-hidden');
-    error.classList.remove('is-hidden');
 
         Notify.failure('Oops! Something went wrong! Try reloading the page or select another cat breed!', {
             position: 'center-center',
